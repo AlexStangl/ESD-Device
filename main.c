b7f48c2bcc51c42ac010b7f48ddf3d04e7b2c8d3
astangl97 <38114749+astangl97@users.noreply.github.com>
2019-03-29 11:38:39 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include "system_LPC11xx.h"
 #include "LPC11xx.h"
 
+void TIMER16_0_IRQHandler()
+{
+	
+}
+
+void TIMER16_1_IRQHandler()
+{
+	
+}
+
+void PIOINT0_IRQHandler()
+{
+	
+}
+
 int main()
 {
 	
